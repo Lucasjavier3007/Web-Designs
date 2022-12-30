83dc94a757ecd52b95b02590d54cae0995cd6725
Javier Oviedo <lucasjavoeroviedo@gmail.com>
2022-12-30 18:03:57 -0300

    Actualizacion Viernes

---
+++ Practice/Ejercicio1.php
@@ -0,0 +1,16 @@
+<?php
+echo "Hola Mundo";
+print("Hello World");
+print_r("Hi World");
+
+// estas instrucciones las ultilizamos para imprimir datos en la pantalla
+
+/* esta otra opcion
+es para 
+hacer comentarios 
+largo
+*/
+
+
+
+?>
